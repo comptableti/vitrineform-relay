730cacdb809e55d3173f897acff7597f57328522
mohas191 <60118548+mohas191@users.noreply.github.com>
2025-06-18 14:04:22 -0400

    vitrineform relay done

---
+++ index.js
@@ -0,0 +1,39 @@
+import express, { Router } from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+app.use(express.json());
+
+app.use('/', async (req, res) => {
+    res.json({
+        message: 'Welcome to the Proxy Server',
+        status: 'Running'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Proxy listening on port', PORT);
+});
+
+const relays = new Router();
+relays.post("/vitrineform", async (req, res) => {
+
+ const targetUrl = 'http://54.39.96.18:3003/emails/send/vitrineform';
+  try {
+    const proxyRes = await fetch(targetUrl, {
+      method: req.method,
+      headers: req.headers,
+      body: req.body,
+    });
+
+    const body = await proxyRes.text();
+    res.status(proxyRes.status).send(body);
+  } catch (err) {
+    res.status(500).send('Proxy error: ' + err.message);
+  }
+
+});
+
+
+app.use('/relays', relays);
